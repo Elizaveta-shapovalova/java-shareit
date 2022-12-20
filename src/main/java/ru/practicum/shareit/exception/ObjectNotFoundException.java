83b4bfbde5112508e934ffefb2a156cbd63ff367
Elizaveta Shapovalova <elizaveta-shapovalova.sha@yandex.ru>
2022-12-20 06:52:40 +0300

    fix codestyle

--- src/main/java/ru/practicum/shareit/exception/ObjectNotFoundException.java
+++ src/main/java/ru/practicum/shareit/exception/ObjectNotFoundException.java
@@ -2,7 +2,7 @@ package ru.practicum.shareit.exception;
 
 public class ObjectNotFoundException extends RuntimeException {
 
-    public ObjectNotFoundException(String message){
+    public ObjectNotFoundException(String message) {
         super(message);
     }
 }
