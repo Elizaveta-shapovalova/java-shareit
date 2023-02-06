295b4ebc44da794c7ebdfa27d904a73d01daffe4
Elizaveta Shapovalova <elizaveta-shapovalova.sha@yandex.ru>
2023-02-06 20:52:33 +0400

    7

--- gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -1,62 +1,63 @@
 package ru.practicum.shareit.booking;
 
+import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import lombok.experimental.FieldDefaults;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.booking.dto.BookItemRequestDto;
 import ru.practicum.shareit.booking.dto.BookingState;
+import ru.practicum.shareit.validation.Create;
 
-import javax.validation.Valid;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.PositiveOrZero;
 
 @Controller
 @RequestMapping(path = "/bookings")
 @RequiredArgsConstructor
-@Slf4j
 @Validated
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class BookingController {
-    private final BookingClient bookingClient;
+    BookingClient bookingClient;
 
     @GetMapping
-    public ResponseEntity<Object> getBookings(@RequestHeader("X-Sharer-User-Id") long userId,
-                                              @RequestParam(name = "state", defaultValue = "all") String stateParam,
-                                              @PositiveOrZero @RequestParam(name = "from", defaultValue = "0") Integer from,
-                                              @Positive @RequestParam(name = "size", defaultValue = "10") Integer size) {
+    public ResponseEntity<Object> getAllByUser(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                               @RequestParam(name = "state", defaultValue = "all") String stateParam,
+                                               @PositiveOrZero @RequestParam(name = "from", defaultValue = "0") Integer from,
+                                               @Positive @RequestParam(name = "size", defaultValue = "10") Integer size) {
         BookingState state = BookingState.from(stateParam);
-        return bookingClient.getBookings(userId, state, from, size);
+        return bookingClient.getAllByUser(userId, state, from, size);
     }
 
     @GetMapping("/owner")
-    public ResponseEntity<Object> getBookingCurrentOwner(@RequestHeader("X-Sharer-User-Id") long userId,
-                                                         @RequestParam(name = "state", defaultValue = "all") String stateParam,
-                                                         @PositiveOrZero @RequestParam(name = "from", defaultValue = "0")
-                                                             Integer from,
-                                                         @Positive @RequestParam(name = "size", defaultValue = "10")
-                                                             Integer size) {
+    public ResponseEntity<Object> getAllByOwner(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                                @RequestParam(name = "state", defaultValue = "all") String stateParam,
+                                                @PositiveOrZero @RequestParam(name = "from", defaultValue = "0")
+                                                Integer from,
+                                                @Positive @RequestParam(name = "size", defaultValue = "10")
+                                                Integer size) {
         BookingState state = BookingState.from(stateParam);
-        return bookingClient.getBookingCurrentOwner(userId, state, from, size);
+        return bookingClient.getAllByOwner(userId, state, from, size);
     }
 
     @PostMapping
-    public ResponseEntity<Object> bookItem(@RequestHeader("X-Sharer-User-Id") long userId,
-                                           @RequestBody @Valid BookItemRequestDto requestDto) {
-        return bookingClient.bookItem(userId, requestDto);
+    public ResponseEntity<Object> create(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                         @Validated({Create.class}) @RequestBody BookItemRequestDto requestDto) {
+        return bookingClient.create(userId, requestDto);
     }
 
     @GetMapping("/{bookingId}")
-    public ResponseEntity<Object> getBooking(@RequestHeader("X-Sharer-User-Id") long userId,
-                                             @PathVariable Long bookingId) {
-        return bookingClient.getBooking(userId, bookingId);
+    public ResponseEntity<Object> getById(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                          @PathVariable Long bookingId) {
+        return bookingClient.getById(userId, bookingId);
     }
 
     @PatchMapping("/{bookingId}")
-    public ResponseEntity<Object> approveStatus(@RequestHeader("X-Sharer-User-Id") long userId,
-                                                @PathVariable Long bookingId,
-                                                @RequestParam boolean approved) {
-        return bookingClient.approveStatus(userId, bookingId, approved);
+    public ResponseEntity<Object> confirmRequest(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                                 @PathVariable Long bookingId,
+                                                 @RequestParam Boolean approved) {
+        return bookingClient.confirmRequest(userId, bookingId, approved);
     }
 }
--- gateway/src/main/java/ru/practicum/shareit/booking/dto/BookItemRequestDto.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/BookItemRequestDto.java
@@ -1,21 +1,26 @@
 package ru.practicum.shareit.booking.dto;
 
-import java.time.LocalDateTime;
-
-import javax.validation.constraints.Future;
-import javax.validation.constraints.FutureOrPresent;
-
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import ru.practicum.shareit.validation.Create;
+
+import javax.validation.constraints.Future;
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class BookItemRequestDto {
-    private long itemId;
-    @FutureOrPresent
-    private LocalDateTime start;
-    @Future
-    private LocalDateTime end;
+    @NotNull(groups = {Create.class})
+    Long itemId;
+    @FutureOrPresent(groups = {Create.class})
+    LocalDateTime start;
+    @Future(groups = {Create.class})
+    LocalDateTime end;
 }
